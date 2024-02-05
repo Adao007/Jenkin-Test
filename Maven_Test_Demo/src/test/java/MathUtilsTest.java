c65e1d2cd7d9532dbaa73c76a1f93a84144a4db9
Adao <anthonydaoy@gmail.com>
2024-02-04 23:03:56 -0800

    Removed classes Runner

--- Maven_Test_Demo/src/test/java/MathUtilsTest.java
+++ Maven_Test_Demo/src/test/java/MathUtilsTest.java
@@ -1,10 +1,8 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
 
-@RunWith(JUnitPlatform.class)
+
 class MathUtilsTest {
 
 	// Unit test for Add method
