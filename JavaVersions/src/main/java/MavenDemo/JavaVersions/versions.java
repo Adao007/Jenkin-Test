77abb08719493f69ed6167228dea2957c81a63ab
Adao <anthonydaoy@gmail.com>
2024-02-23 23:14:23 -0800

    "adding the Maven Java Version Tests"

---
+++ JavaVersions/src/main/java/MavenDemo/JavaVersions/versions.java
@@ -0,0 +1,11 @@
+package MavenDemo.JavaVersions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class versions {
+	public static void main( String[] args) {
+		System.out.println("Hello, world"); 
+	}
+}
